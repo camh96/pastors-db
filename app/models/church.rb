1fb0afd345593f1271656331ef44b7697b177400
Michael Brown <redhoodie@gmail.com>
2016-08-25 15:36:59 +1200

    Annotate models.

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: churches
+#
+#  id           :integer          not null, primary key
+#  church_name  :string
+#  suburb       :string
+#  postcode     :integer
+#  city         :string
+#  address      :string
+#  website      :string
+#  landline_tel :string
+#  active       :boolean          default(TRUE), not null
+#  pastorscoop  :boolean          default(FALSE), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Church < ActiveRecord::Base
   has_many :people
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  church_id  :integer
+#  conference :boolean
+#  mobile_tel :string
+#  notes      :text
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Person < ActiveRecord::Base
   belongs_to :church
 
